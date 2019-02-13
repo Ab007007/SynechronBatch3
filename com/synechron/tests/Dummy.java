bca9f6a91fe1699341179cfe99d727f6152c14a6
AravindaHB <aru03.info@gmail.com>
2019-02-13 09:20:20 +0530

    fluent wait

---
+++ com/synechron/tests/Dummy.java
@@ -0,0 +1,18 @@
+package com.synechron.tests;
+
+import java.util.function.Function;
+
+public class Dummy 
+{
+	public static void main(String[] args) {
+		Function<Integer, String> fun = new Function<Integer, String>() {
+
+			@Override
+			public String apply(Integer t) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+
+}
---
+++ com/synechron/waits/FluentWaitDemo.java
@@ -0,0 +1,65 @@
+package com.synechron.waits;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import com.google.common.collect.FluentIterable;
+import com.synechron.utils.ActitimeUtils;
+import com.synechron.utils.DriverUtils;
+
+public class FluentWaitDemo
+{
+	public static void main(String[] args) {
+		WebDriver driver = DriverUtils.getMyDriver();
+		ActitimeUtils.launchApp(driver, "file:///D:/Synechron/11_FEB_SELENIUM/html/CounterText.html");
+		
+		FluentWait<WebElement> wait  = 
+				new FluentWait<WebElement>(driver.findElement(By.id("counter")))
+				.withTimeout(Duration.ofSeconds(10)).pollingEvery(Duration.ofMillis(100))
+				.ignoring(NoSuchElementException.class)
+				.ignoring(Exception.class);
+		
+		Function<WebElement, Boolean> fun = new Function<WebElement, Boolean>() {
+
+			@Override
+			public Boolean apply(WebElement myele) {
+				String text =null;
+				text = myele.getText();
+				if(!(text.equals("NaN")))
+				{
+					System.out.println(" Waiting for Element to change the status...");
+					return false;
+				}
+				else
+				{
+					System.out.println("found..!!!!");
+					return true;
+				}
+			}
+		};
+	
+		wait.until(fun);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
